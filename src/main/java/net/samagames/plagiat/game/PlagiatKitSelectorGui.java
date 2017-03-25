98d5c328449112579533f615539f43d75ec2438a
Rigner <frasse_l@epitech.eu>
2017-03-25 23:21:58 +0100

    Changed color

--- src/main/java/net/samagames/plagiat/game/PlagiatKitSelectorGui.java
+++ src/main/java/net/samagames/plagiat/game/PlagiatKitSelectorGui.java
@@ -4,6 +4,7 @@ import net.samagames.api.gui.AbstractGui;
 import net.samagames.api.shops.IPlayerShop;
 import net.samagames.plagiat.Plagiat;
 import net.samagames.tools.GlowEffect;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -45,7 +46,7 @@ public class PlagiatKitSelectorGui extends AbstractGui
 
         ItemStack arrow = new ItemStack(Material.ARROW);
         ItemMeta arrowMeta = arrow.getItemMeta();
-        arrowMeta.setDisplayName("Retour");
+        arrowMeta.setDisplayName(ChatColor.GREEN + "Retour");
         arrow.setItemMeta(arrowMeta);
         this.setSlotData(arrow, 26, "back");
 
