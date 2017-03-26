6640fd6380f14457b5553519d0da6b9813d4e6e0
Rigner <frasse_l@epitech.eu>
2017-03-26 01:05:55 +0100

    Fixed wools particles

--- src/main/java/net/samagames/plagiat/modules/sheepwars/SheepWarsModule.java
+++ src/main/java/net/samagames/plagiat/modules/sheepwars/SheepWarsModule.java
@@ -113,7 +113,7 @@ public class SheepWarsModule extends AbstractModule
      */
     private void spawnParticles()
     {
-        this.woolLocations.forEach(location -> location.getWorld().spawnParticle(Particle.PORTAL, location.clone().add(0.5D, 0.5D, 0.5D), 3, 0.5F, 0.5D, 0.5D, 0.5D));
+        this.woolLocations.stream().filter(location -> location.getBlock().getType() != Material.AIR).forEach(location -> location.getWorld().spawnParticle(Particle.PORTAL, location.clone().add(0.5D, 0.5D, 0.5D), 3, 0.5F, 0.5D, 0.5D, 0.5D));
     }
 
     /**
